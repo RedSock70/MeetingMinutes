2332bfa79d09827cb2d56a4e702ab539be723b49
RedSock70 <Benhurst04@outlook.com>
2022-08-10 08:29:51 +1200

    update
    update

--- Website/auth.py
+++ Website/auth.py
@@ -1,4 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p> Logout </p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        FirstName = request.form.get('FirstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 3 Characters.', category='error')
+        elif len(FirstName) < 2:
+            flash('First name must be greater than 1 Characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 7 Characters.', category='error')
+        else:
+            flash('Account Created!', category='success')
+            
+            # add user to database 
+            return render_template("sign_up.html")
--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from Website import create_app
 
 app = create_app()
 
-# @app.route("/",methods = ['GET','POST'])
-# def index():
-#     return render_template("index.html")
-
 if __name__ == '__main__':
     app.run(debug=True)
+    
